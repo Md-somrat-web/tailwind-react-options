3a1cff00a4f024ebf084c05b1f419dac0a332217
md somrat islam <islammdsomrat05@gmail.com>
2025-04-12 20:55:13 +0600

    tailwind,diasyui, with react

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import DaisyNav from "./componentens/DaisyNav/DaisyNav";
+
+function App() {
+  return (
+    <>
+      <header>
+        <DaisyNav></DaisyNav>
+      </header>
+      <main></main>
+    </>
+  );
+}
+
+export default App;
